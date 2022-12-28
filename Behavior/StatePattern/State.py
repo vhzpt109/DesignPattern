3c9f3c53ff4ca26376de6e439796a0cccb42026d
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-12-29 01:55:35 +0900

    State Pattern

--- Behavior/StatePattern/State.py
+++ Behavior/StatePattern/State.py
@@ -1,27 +1,47 @@
 class TrafficLight:
     def __init__(self):
-        self.state = 'green'
+        self.state = GreenLight()
 
-    # prefer enum
-    def setState(self, state: str):
+    def setState(self, state):
         self.state = state
 
     def speak(self):
-        if self.state == 'green':
-            print('green light')
-        elif self.state == 'red':
-            print('red light')
+        self.state.speak()
 
-    def wait(self):
-        print('wait.. the light changed')
-        if self.state == 'green':
-            self.state = 'red'
-        elif self.state == 'red':
-            self.state = 'green'
+    def change(self):
+        self.state.change(self)
+
+
+class StateInterface:
+    def speak(self):
+        pass
+
+    def change(self, traffit_light: TrafficLight):
+        pass
+
+
+class GreenLight(StateInterface):
+    def speak(self):
+        print("green light")
+
+    def change(self, traffit_light: TrafficLight):
+        print("change state")
+        traffit_light.setState(RedLight())
+
+
+class RedLight(StateInterface):
+    def speak(self):
+        print("red light")
+
+    def change(self, traffit_light: TrafficLight):
+        print("change state")
+        traffit_light.setState(GreenLight())
 
 
 if __name__ == "__main__":
     t_light = TrafficLight()
     t_light.speak()
-    t_light.wait()
+
+    t_light.change()
+
     t_light.speak()
