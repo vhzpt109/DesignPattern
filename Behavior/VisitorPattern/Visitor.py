f2278401608c3b749bc31f7c6e1a0fcb38082e91
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-01-27 21:42:22 +0900

    Code Refactoring

--- Behavior/VisitorPattern/Visitor.py
+++ Behavior/VisitorPattern/Visitor.py
@@ -1,34 +1,43 @@
 class AnimalVisitor:
-  def catVisit(self,elem):
-    pass
-  def dogVisit(self,elem):
-    pass
+    def catVisit(self, elem):
+        pass
+
+    def dogVisit(self, elem):
+        pass
+
 
 class SpeakVisitor(AnimalVisitor):
-  def catVisit(self,elem):
-    print("meow~")
-  def dogVisit(self,elem):
-    print("bark!")
+    def catVisit(self, elem):
+        print("meow~")
+
+    def dogVisit(self, elem):
+        print("bark!")
+
 
 class NameVisitor(AnimalVisitor):
-  def catVisit(self,elem):
-    print(f"cat, {elem.name}")
-  def dogVisit(self,elem):
-    print(f"dog, {elem.name}")
+    def catVisit(self, elem):
+        print(f"cat, {elem.name}")
+
+    def dogVisit(self, elem):
+        print(f"dog, {elem.name}")
+
 
 class Animal:
-  def __init__(self,name:str):
-    self.name = name
-  def accept(self,visitor:AnimalVisitor):
-    pass
+    def __init__(self, name: str):
+        self.name = name
+
+    def accept(self, visitor: AnimalVisitor):
+        pass
+
 
 class Cat(Animal):
-  def accept(self,visitor:AnimalVisitor):
-    visitor.catVisit(self)
+    def accept(self, visitor: AnimalVisitor):
+        visitor.catVisit(self)
+
 
 class Dog(Animal):
-  def accept(self,visitor:AnimalVisitor):
-    visitor.dogVisit(self)
+    def accept(self, visitor: AnimalVisitor):
+        visitor.dogVisit(self)
 
 
 if __name__ == "__main__":
