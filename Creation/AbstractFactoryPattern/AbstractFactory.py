aa519ddcb571d4107582e1d57c8a6c5640063a04
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-02-05 13:04:52 +0900

    Abstract Factory Pattern

--- Creation/AbstractFactoryPattern/AbstractFactory.py
+++ Creation/AbstractFactoryPattern/AbstractFactory.py
@@ -1,85 +1,157 @@
-class Button:
-    def click(self):
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class AbstractFactory(ABC):
+    """
+    The Abstract Factory interface declares a set of methods that return
+    different abstract products. These products are called a family and are
+    related by a high-level theme or concept. Products of one family are usually
+    able to collaborate among themselves. A family of products may have several
+    variants, but the products of one variant are incompatible with products of
+    another.
+    """
+    @abstractmethod
+    def create_product_a(self) -> AbstractProductA:
+        pass
+
+    @abstractmethod
+    def create_product_b(self) -> AbstractProductB:
         pass
 
 
-class DarkButton(Button):
-    def click(self):
-        print("dark click")
+class ConcreteFactory1(AbstractFactory):
+    """
+    Concrete Factories produce a family of products that belong to a single
+    variant. The factory guarantees that resulting products are compatible. Note
+    that signatures of the Concrete Factory's methods return an abstract
+    product, while inside the method a concrete product is instantiated.
+    """
+
+    def create_product_a(self) -> AbstractProductA:
+        return ConcreteProductA1()
+
+    def create_product_b(self) -> AbstractProductB:
+        return ConcreteProductB1()
 
 
-class LightButton(Button):
-    def click(self):
-        print("light click")
+class ConcreteFactory2(AbstractFactory):
+    """
+    Each Concrete Factory has a corresponding product variant.
+    """
+
+    def create_product_a(self) -> AbstractProductA:
+        return ConcreteProductA2()
+
+    def create_product_b(self) -> AbstractProductB:
+        return ConcreteProductB2()
 
 
-class CheckBox:
-    def check(self):
+class AbstractProductA(ABC):
+    """
+    Each distinct product of a product family should have a base interface. All
+    variants of the product must implement this interface.
+    """
+
+    @abstractmethod
+    def useful_function_a(self) -> str:
         pass
 
 
-class DarkCheckBox(CheckBox):
-    def check(self):
-        print("dark check")
+"""
+Concrete Products are created by corresponding Concrete Factories.
+"""
 
 
-class LightCheckBox(CheckBox):
-    def check(self):
-        print("light check")
+class ConcreteProductA1(AbstractProductA):
+    def useful_function_a(self) -> str:
+        return "The result of the product A1."
 
 
-class ScrollBar:
-  def scroll(self):
-    pass
-
-class DarkScrollBar(ScrollBar):
-  def scroll(self):
-    print("dark scroll")
-
-class LightScrollBar(ScrollBar):
-  def scroll(self):
-    print("light scroll")
+class ConcreteProductA2(AbstractProductA):
+    def useful_function_a(self) -> str:
+        return "The result of the product A2."
 
 
-class UIFactory:
-    def getButton(self):
+class AbstractProductB(ABC):
+    """
+    Here's the the base interface of another product. All products can interact
+    with each other, but proper interaction is possible only between products of
+    the same concrete variant.
+    """
+    @abstractmethod
+    def useful_function_b(self) -> None:
+        """
+        Product B is able to do its own thing...
+        """
         pass
 
-    def getCheckBox(self):
+    @abstractmethod
+    def another_useful_function_b(self, collaborator: AbstractProductA) -> None:
+        """
+        ...but it also can collaborate with the ProductA.
+
+        The Abstract Factory makes sure that all products it creates are of the
+        same variant and thus, compatible.
+        """
         pass
 
-    def getScrollBar(self):
-        pass
+
+"""
+Concrete Products are created by corresponding Concrete Factories.
+"""
 
 
-class DarkUIFactory(UIFactory):
-    def getButton(self):
-        return DarkButton()
+class ConcreteProductB1(AbstractProductB):
+    def useful_function_b(self) -> str:
+        return "The result of the product B1."
 
-    def getCheckBox(self):
-        return DarkCheckBox()
+    """
+    The variant, Product B1, is only able to work correctly with the variant,
+    Product A1. Nevertheless, it accepts any instance of AbstractProductA as an
+    argument.
+    """
 
-    def getScrollBar(self):
-        return DarkScrollBar()
+    def another_useful_function_b(self, collaborator: AbstractProductA) -> str:
+        result = collaborator.useful_function_a()
+        return f"The result of the B1 collaborating with the ({result})"
 
 
-class LightUIFactory(UIFactory):
-    def getButton(self):
-        return LightButton()
+class ConcreteProductB2(AbstractProductB):
+    def useful_function_b(self) -> str:
+        return "The result of the product B2."
 
-    def getCheckBox(self):
-        return LightCheckBox()
+    def another_useful_function_b(self, collaborator: AbstractProductA):
+        """
+        The variant, Product B2, is only able to work correctly with the
+        variant, Product A2. Nevertheless, it accepts any instance of
+        AbstractProductA as an argument.
+        """
+        result = collaborator.useful_function_a()
+        return f"The result of the B2 collaborating with the ({result})"
 
-    def getScrollBar(self):
-        return LightScrollBar()
+
+def client_code(factory: AbstractFactory) -> None:
+    """
+    The client code works with factories and products only through abstract
+    types: AbstractFactory and AbstractProduct. This lets you pass any factory
+    or product subclass to the client code without breaking it.
+    """
+    product_a = factory.create_product_a()
+    product_b = factory.create_product_b()
+
+    print(f"{product_b.useful_function_b()}")
+    print(f"{product_b.another_useful_function_b(product_a)}", end="")
 
 
 if __name__ == "__main__":
-    light_factory = LightUIFactory()
-    light_btn = light_factory.getButton()
-    light_checkBx = light_factory.getCheckBox()
-    light_scrollBar = light_factory.getScrollBar()
+    """
+    The client code can work with any concrete factory class.
+    """
+    print("Client: Testing client code with the first factory type:")
+    client_code(ConcreteFactory1())
 
-    light_btn.click()
-    light_checkBx.check()
-    light_scrollBar.scroll()
+    print("\n")
+
+    print("Client: Testing the same client code with the second factory type:")
+    client_code(ConcreteFactory2())
---
+++ Creation/AbstractFactoryPattern/AbstractFactorySample.py
@@ -0,0 +1,85 @@
+class Button:
+    def click(self):
+        pass
+
+
+class DarkButton(Button):
+    def click(self):
+        print("dark click")
+
+
+class LightButton(Button):
+    def click(self):
+        print("light click")
+
+
+class CheckBox:
+    def check(self):
+        pass
+
+
+class DarkCheckBox(CheckBox):
+    def check(self):
+        print("dark check")
+
+
+class LightCheckBox(CheckBox):
+    def check(self):
+        print("light check")
+
+
+class ScrollBar:
+  def scroll(self):
+    pass
+
+class DarkScrollBar(ScrollBar):
+  def scroll(self):
+    print("dark scroll")
+
+class LightScrollBar(ScrollBar):
+  def scroll(self):
+    print("light scroll")
+
+
+class UIFactory:
+    def getButton(self):
+        pass
+
+    def getCheckBox(self):
+        pass
+
+    def getScrollBar(self):
+        pass
+
+
+class DarkUIFactory(UIFactory):
+    def getButton(self):
+        return DarkButton()
+
+    def getCheckBox(self):
+        return DarkCheckBox()
+
+    def getScrollBar(self):
+        return DarkScrollBar()
+
+
+class LightUIFactory(UIFactory):
+    def getButton(self):
+        return LightButton()
+
+    def getCheckBox(self):
+        return LightCheckBox()
+
+    def getScrollBar(self):
+        return LightScrollBar()
+
+
+if __name__ == "__main__":
+    light_factory = LightUIFactory()
+    light_btn = light_factory.getButton()
+    light_checkBx = light_factory.getCheckBox()
+    light_scrollBar = light_factory.getScrollBar()
+
+    light_btn.click()
+    light_checkBx.check()
+    light_scrollBar.scroll()
