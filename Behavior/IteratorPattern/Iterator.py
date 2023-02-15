bb84fb193353b23317e90e53a357f6d5159b1e36
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-02-15 21:48:49 +0900

    Iterator Pattern

--- Behavior/IteratorPattern/Iterator.py
+++ Behavior/IteratorPattern/Iterator.py
@@ -1,50 +1,88 @@
-class Iterator:
-    def nextVal(self):
-        pass
-
-    def hasNext(self):
-        pass
+from __future__ import annotations
+from collections.abc import Iterable, Iterator
+from typing import Any, List
 
 
-class ArrayIterator(Iterator):
-    def __init__(self, arrayContainer):
-        self.container = arrayContainer.container
-        self.pos = -1
-
-    def hasNext(self):
-        if self.pos < len(self.container) - 1:
-            return True
-        else:
-            return False
-
-    def nextVal(self):
-        if self.hasNext():
-            self.pos += 1
-            return self.container[self.pos]
+"""
+To create an iterator in Python, there are two abstract classes from the built-
+in `collections` module - Iterable,Iterator. We need to implement the
+`__iter__()` method in the iterated object (collection), and the `__next__ ()`
+method in theiterator.
+"""
 
 
-class ArryContainer:
-    def __init__(self):
-        self.container = []
+class AlphabeticalOrderIterator(Iterator):
+    """
+    Concrete Iterators implement various traversal algorithms. These classes
+    store the current traversal position at all times.
+    """
 
-    def add(self, num: int):
-        self.container.append(num)
+    """
+    `_position` attribute stores the current traversal position. An iterator may
+    have a lot of other fields for storing iteration state, especially when it
+    is supposed to work with a particular kind of collection.
+    """
+    _position: int = None
 
-    def getIterator(self):
-        return ArrayIterator(self)
+    """
+    This attribute indicates the traversal direction.
+    """
+    _reverse: bool = False
+
+    def __init__(self, collection: WordsCollection, reverse: bool = False) -> None:
+        self._collection = collection
+        self._reverse = reverse
+        self._position = -1 if reverse else 0
+
+    def __next__(self):
+        """
+        The __next__() method must return the next item in the sequence. On
+        reaching the end, and in subsequent calls, it must raise StopIteration.
+        """
+        try:
+            value = self._collection[self._position]
+            self._position += -1 if self._reverse else 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
 
 
-def printByIter(iter):
-    while iter.hasNext():
-        print(iter.nextVal(), end=' ')
-    print(' ')
+class WordsCollection(Iterable):
+    """
+    Concrete Collections provide one or several methods for retrieving fresh
+    iterator instances, compatible with the collection class.
+    """
+
+    def __init__(self, collection: List[Any] = []) -> None:
+        self._collection = collection
+
+    def __iter__(self) -> AlphabeticalOrderIterator:
+        """
+        The __iter__() method returns the iterator object itself, by default we
+        return the iterator in ascending order.
+        """
+        return AlphabeticalOrderIterator(self._collection)
+
+    def get_reverse_iterator(self) -> AlphabeticalOrderIterator:
+        return AlphabeticalOrderIterator(self._collection, True)
+
+    def add_item(self, item: Any):
+        self._collection.append(item)
 
 
 if __name__ == "__main__":
-    array_container = ArryContainer()
-    array_container.add(1)
-    array_container.add(3)
-    array_container.add(5)
-    iterator = array_container.getIterator()
+    # The client code may or may not know about the Concrete Iterator or
+    # Collection classes, depending on the level of indirection you want to keep
+    # in your program.
+    collection = WordsCollection()
+    collection.add_item("First")
+    collection.add_item("Second")
+    collection.add_item("Third")
 
-    printByIter(iterator)
+    print("Straight traversal:")
+    print("\n".join(collection))
+    print("")
+
+    print("Reverse traversal:")
+    print("\n".join(collection.get_reverse_iterator()), end="")
