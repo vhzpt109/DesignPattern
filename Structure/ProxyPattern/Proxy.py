d9c002594fbc4f147fbc13a23126c4274b72e2d1
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-02-16 22:03:25 +0900

    Proxy Pattern

--- Structure/ProxyPattern/Proxy.py
+++ Structure/ProxyPattern/Proxy.py
@@ -1,28 +1,81 @@
-class Animal:  # interface class
-    def speak(self):
+from abc import ABC, abstractmethod
+
+
+class Subject(ABC):
+    """
+    The Subject interface declares common operations for both RealSubject and
+    the Proxy. As long as the client works with RealSubject using this
+    interface, you'll be able to pass it a proxy instead of a real subject.
+    """
+
+    @abstractmethod
+    def request(self) -> None:
         pass
 
 
-class Cat(Animal):
-    def speak(self):
-        print("meow")
+class RealSubject(Subject):
+    """
+    The RealSubject contains some core business logic. Usually, RealSubjects are
+    capable of doing some useful work which may also be very slow or sensitive -
+    e.g. correcting input data. A Proxy can solve these issues without any
+    changes to the RealSubject's code.
+    """
+
+    def request(self) -> None:
+        print("RealSubject: Handling request.")
 
 
-class CatProxy(Animal):
-    def __init__(self, cat: Cat):
-        self.cat = cat
+class Proxy(Subject):
+    """
+    The Proxy has an interface identical to the RealSubject.
+    """
 
-    def speak(self):
-        print("before speak")
-        self.cat.speak()
-        print("after speak")
+    def __init__(self, real_subject: RealSubject) -> None:
+        self._real_subject = real_subject
+
+    def request(self) -> None:
+        """
+        The most common applications of the Proxy pattern are lazy loading,
+        caching, controlling the access, logging, etc. A Proxy can perform one
+        of these things and then, depending on the result, pass the execution to
+        the same method in a linked RealSubject object.
+        """
+
+        if self.check_access():
+            self._real_subject.request()
+            self.log_access()
+
+    def check_access(self) -> bool:
+        print("Proxy: Checking access prior to firing a real request.")
+        return True
+
+    def log_access(self) -> None:
+        print("Proxy: Logging the time of request.", end="")
 
 
-def doSpeak(animal: Animal):
-    animal.speak()
+def client_code(subject: Subject) -> None:
+    """
+    The client code is supposed to work with all objects (both subjects and
+    proxies) via the Subject interface in order to support both real subjects
+    and proxies. In real life, however, clients mostly work with their real
+    subjects directly. In this case, to implement the pattern more easily, you
+    can extend your proxy from the real subject's class.
+    """
+
+    # ...
+
+    subject.request()
+
+    # ...
 
 
 if __name__ == "__main__":
-    kitty = Cat()
-    kitty_proxy = CatProxy(kitty)
-    doSpeak(kitty_proxy)
+    print("Client: Executing the client code with a real subject:")
+    real_subject = RealSubject()
+    client_code(real_subject)
+
+    print("")
+
+    print("Client: Executing the same client code with a proxy:")
+    proxy = Proxy(real_subject)
+    client_code(proxy)
