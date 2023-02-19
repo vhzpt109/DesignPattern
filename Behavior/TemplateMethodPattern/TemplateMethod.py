868fd5d3ae9b8f36bf8a3b0144d61d5478704e7c
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-02-19 22:57:10 +0900

    Template Method Pattern

--- Behavior/TemplateMethodPattern/TemplateMethod.py
+++ Behavior/TemplateMethodPattern/TemplateMethod.py
@@ -1,42 +1,107 @@
-class BasicRamenRecipe:
-    def cookRamen(self):
-        self.boilWater()
-        self.addRamen()
-        self.addons()
-        self.wait()
+from abc import ABC, abstractmethod
 
-    def boilWater(self):
-        print("boil 550ml of water")
 
-    def addRamen(self):
-        print("add noodles, soup Base, flakes")
+class AbstractClass(ABC):
+    """
+    The Abstract Class defines a template method that contains a skeleton of
+    some algorithm, composed of calls to (usually) abstract primitive
+    operations.
 
-    def addons(self):
+    Concrete subclasses should implement these operations, but leave the
+    template method itself intact.
+    """
+
+    def template_method(self) -> None:
+        """
+        The template method defines the skeleton of an algorithm.
+        """
+
+        self.base_operation1()
+        self.required_operations1()
+        self.base_operation2()
+        self.hook1()
+        self.required_operations2()
+        self.base_operation3()
+        self.hook2()
+
+    # These operations already have implementations.
+
+    def base_operation1(self) -> None:
+        print("AbstractClass says: I am doing the bulk of the work")
+
+    def base_operation2(self) -> None:
+        print("AbstractClass says: But I let subclasses override some operations")
+
+    def base_operation3(self) -> None:
+        print("AbstractClass says: But I am doing the bulk of the work anyway")
+
+    # These operations have to be implemented in subclasses.
+
+    @abstractmethod
+    def required_operations1(self) -> None:
         pass
 
-    def wait(self):
-        print("cook for 4min 30s")
+    @abstractmethod
+    def required_operations2(self) -> None:
+        pass
+
+    # These are "hooks." Subclasses may override them, but it's not mandatory
+    # since the hooks already have default (but empty) implementation. Hooks
+    # provide additional extension points in some crucial places of the
+    # algorithm.
+
+    def hook1(self) -> None:
+        pass
+
+    def hook2(self) -> None:
+        pass
 
 
-class NocopeRecipe(BasicRamenRecipe):
-    def addons(self):
-        print("add onions")
+class ConcreteClass1(AbstractClass):
+    """
+    Concrete classes have to implement all abstract operations of the base
+    class. They can also override some operations with a default implementation.
+    """
+
+    def required_operations1(self) -> None:
+        print("ConcreteClass1 says: Implemented Operation1")
+
+    def required_operations2(self) -> None:
+        print("ConcreteClass1 says: Implemented Operation2")
 
 
-class GrandmaRecipe(BasicRamenRecipe):
-    def boilWater(self):
-        print("boil 1000ml of water")
+class ConcreteClass2(AbstractClass):
+    """
+    Usually, concrete classes override only a fraction of base class'
+    operations.
+    """
 
-    def wait(self):
-        print("cook for 10m")
+    def required_operations1(self) -> None:
+        print("ConcreteClass2 says: Implemented Operation1")
+
+    def required_operations2(self) -> None:
+        print("ConcreteClass2 says: Implemented Operation2")
+
+    def hook1(self) -> None:
+        print("ConcreteClass2 says: Overridden Hook1")
+
+
+def client_code(abstract_class: AbstractClass) -> None:
+    """
+    The client code calls the template method to execute the algorithm. Client
+    code does not have to know the concrete class of an object it works with, as
+    long as it works with objects through the interface of their base class.
+    """
+
+    # ...
+    abstract_class.template_method()
+    # ...
 
 
 if __name__ == "__main__":
-    basicRecipe = BasicRamenRecipe()
-    basicRecipe.cookRamen()
+    print("Same client code can work with different subclasses:")
+    client_code(ConcreteClass1())
+    print("")
 
-    nocopeRecipe = NocopeRecipe()
-    nocopeRecipe.cookRamen()
-
-    granmaRecipe = GrandmaRecipe()
-    granmaRecipe.cookRamen()
+    print("Same client code can work with different subclasses:")
+    client_code(ConcreteClass2())
