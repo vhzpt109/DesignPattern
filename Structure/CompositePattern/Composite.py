ba85c9090ca2c32fb2662b93df20c8135c76052a
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-02-20 22:39:23 +0900

    Composite Pattern

--- Structure/CompositePattern/Composite.py
+++ Structure/CompositePattern/Composite.py
@@ -1,61 +1,158 @@
-# class Component:
-#     def fn(self):
-#         pass
-#
-#
-# class Leaf(Component):
-#     def fn(self):
-#         print('leaf')
-#
-# 
-# class Composite(Component):
-#     def __init__(self):
-#         self.components = []
-#
-#     def add(self, component: Component):
-#         self.components.append(component)
-#
-#     def fn(self):
-#         print('composite')
-#         for component in self.components:
-#             component.fn()
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
 
-class Animal:
-  def speak(self):
-    pass
 
-class Cat(Animal):
-  def speak(self):
-    print("meow")
+class Component(ABC):
+    """
+    The base Component class declares common operations for both simple and
+    complex objects of a composition.
+    """
 
-class Dog(Animal):
-  def speak(self):
-    print("bark")
+    @property
+    def parent(self) -> Component:
+        return self._parent
 
-class AnimalGroup(Animal):
-  def __init__(self):
-    self.animals = []
+    @parent.setter
+    def parent(self, parent: Component):
+        """
+        Optionally, the base Component can declare an interface for setting and
+        accessing a parent of the component in a tree structure. It can also
+        provide some default implementation for these methods.
+        """
 
-  def add(self,animal:Animal):
-    self.animals.append(animal)
+        self._parent = parent
 
-  def speak(self):
-    print("group speaking..")
-    for animal in self.animals:
-      animal.speak()
+    """
+    In some cases, it would be beneficial to define the child-management
+    operations right in the base Component class. This way, you won't need to
+    expose any concrete component classes to the client code, even during the
+    object tree assembly. The downside is that these methods will be empty for
+    the leaf-level components.
+    """
+
+    def add(self, component: Component) -> None:
+        pass
+
+    def remove(self, component: Component) -> None:
+        pass
+
+    def is_composite(self) -> bool:
+        """
+        You can provide a method that lets the client code figure out whether a
+        component can bear children.
+        """
+
+        return False
+
+    @abstractmethod
+    def operation(self) -> str:
+        """
+        The base Component may implement some default behavior or leave it to
+        concrete classes (by declaring the method containing the behavior as
+        "abstract").
+        """
+
+        pass
+
+
+class Leaf(Component):
+    """
+    The Leaf class represents the end objects of a composition. A leaf can't
+    have any children.
+
+    Usually, it's the Leaf objects that do the actual work, whereas Composite
+    objects only delegate to their sub-components.
+    """
+
+    def operation(self) -> str:
+        return "Leaf"
+
+
+class Composite(Component):
+    """
+    The Composite class represents the complex components that may have
+    children. Usually, the Composite objects delegate the actual work to their
+    children and then "sum-up" the result.
+    """
+
+    def __init__(self) -> None:
+        self._children: List[Component] = []
+
+    """
+    A composite object can add or remove other components (both simple or
+    complex) to or from its child list.
+    """
+
+    def add(self, component: Component) -> None:
+        self._children.append(component)
+        component.parent = self
+
+    def remove(self, component: Component) -> None:
+        self._children.remove(component)
+        component.parent = None
+
+    def is_composite(self) -> bool:
+        return True
+
+    def operation(self) -> str:
+        """
+        The Composite executes its primary logic in a particular way. It
+        traverses recursively through all its children, collecting and summing
+        their results. Since the composite's children pass these calls to their
+        children and so forth, the whole object tree is traversed as a result.
+        """
+
+        results = []
+        for child in self._children:
+            results.append(child.operation())
+        return f"Branch({'+'.join(results)})"
+
+
+def client_code(component: Component) -> None:
+    """
+    The client code works with all of the components via the base interface.
+    """
+
+    print(f"RESULT: {component.operation()}", end="")
+
+
+def client_code2(component1: Component, component2: Component) -> None:
+    """
+    Thanks to the fact that the child-management operations are declared in the
+    base Component class, the client code can work with any component, simple or
+    complex, without depending on their concrete classes.
+    """
+
+    if component1.is_composite():
+        component1.add(component2)
+
+    print(f"RESULT: {component1.operation()}", end="")
+
 
 if __name__ == "__main__":
-    cat_group = AnimalGroup()
-    cat_group.add(Cat())
-    cat_group.add(Cat())
-    cat_group.add(Cat())
+    # This way the client code can support the simple leaf components...
+    simple = Leaf()
+    print("Client: I've got a simple component:")
+    client_code(simple)
+    print("\n")
 
-    dog_group = AnimalGroup()
-    dog_group.add(Dog())
-    dog_group.add(Dog())
+    # ...as well as the complex composites.
+    tree = Composite()
 
-    zoo = AnimalGroup()
-    zoo.add(cat_group)
-    zoo.add(dog_group)
+    branch1 = Composite()
+    branch1.add(Leaf())
+    branch1.add(Leaf())
 
-    zoo.speak()
+    branch2 = Composite()
+    branch2.add(Leaf())
+
+    tree.add(branch1)
+    tree.add(branch2)
+
+    print("Client: Now I've got a composite tree:")
+    client_code(tree)
+    print("\n")
+
+    print("Client: I don't need to check the components classes even when managing the tree:")
+    client_code2(tree, simple)
